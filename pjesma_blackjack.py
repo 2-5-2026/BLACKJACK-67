8b493c68eaf643d3a830f50d0368056abed99067
lukamilani-prog <luka.milani@skole.hr>
2026-04-09 22:33:33 +0200

    Update pjesma_blackjack.py

--- pjesma_blackjack.py
+++ pjesma_blackjack.py
@@ -1,3 +1,3 @@
 pygame.mixer.init()
-pygame.mixer.music.load("C:/Users/luxio/OneDrive/Documents/Piton/music.mp3")  # stavi svoj MP3
+pygame.mixer.music.load("C:/Users/luxio/OneDrive/Documents/Piton/music.mp3") 
 pygame.mixer.music.play(-1)
